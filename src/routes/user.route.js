80139ca7ba5db75991b31fd83ff76a6a9019c36d
Indrani-Paul <paul.indrani999@gmail.com>
2024-08-24 13:19:14 +0530

    fixed the issue of bcrypt

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const { TaskUserModel } = require('../model/user.model');
 const userRouter=express.Router();
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt=require("jsonwebtoken");
 require('dotenv').config();
 
